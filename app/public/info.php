8dde48f93d5efc97e17310aebe23a646f1c170f4
Philip Schröder <post@macphil.de>
2025-05-01 22:37:07 +0200

    Adds info endpoint for request details
    Adds an endpoint that returns request details as JSON.
    This facilitates debugging and understanding request parameters.

--- app/public/info.php
+++ app/public/info.php
@@ -1,5 +1,17 @@
 <?php
-
-phpinfo(INFO_GENERAL + INFO_MODULES + INFO_VARIABLES);
+if(isset($_REQUEST)){
+    header("Content-Type: application/json");
+    $response = array(
+        "_REQUEST" => $_REQUEST,
+        "_GET" => $_GET,
+        "_POST" => $_POST,
+        "BODY" => file_get_contents('php://input'), 
+        "_SERVER" => $_SERVER
+    );
+    echo json_encode($response, JSON_UNESCAPED_SLASHES );
+}
+else {
+    phpinfo(INFO_GENERAL + INFO_MODULES + INFO_VARIABLES);
+}
 
 ?>
